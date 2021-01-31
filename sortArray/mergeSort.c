fe079fee23f949ec2491f91888b7c7f0a333ff19
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2021-01-31 19:04:03 -0300

    Pequenas refatorações

--- sortArray/mergeSort.c
+++ sortArray/mergeSort.c
@@ -6,12 +6,11 @@ void swap(int *elementA, int *elementB) {
   *elementB = aux;
 }
 
-
 int main(int argc, char const *argv[]) {
   int array[] = {23, 432423, 123, 231545, 21335, 534534, 36, 542435, 523452345, 643, 132, 432, 123, 543, 9876, 345, 324, 456, 3456, 523534, 34, 23, 34, 53, 4534, 5, 345, 435, 2, 5, 7, 5, 4546, 3, 5, 6534, 54};
-  int arraySize= sizeof(array) / sizeof(int);
+  int arraySize = sizeof(array) / sizeof(int);
 
-
+  printf("%d", arraySize);
   printf("Elementos em Ordem:\n");
   for (int i = 0; i < arraySize; i++) {
     printf("e[%i]: %d\n", i, array[i]);
