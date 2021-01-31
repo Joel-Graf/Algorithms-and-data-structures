c3d3b2e33c0004a73b6c26320d9411259e16ccd5
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2021-01-31 20:52:45 -0300

    implementado a função merge

--- sortArray/mergeSort.c
+++ sortArray/mergeSort.c
@@ -6,11 +6,49 @@ void swap(int *elementA, int *elementB) {
   *elementB = aux;
 }
 
+void merge(int *array, int start, int end) {
+  int middle = (start + end) / 2;
+  int tempArray[end - start];
+  
+  int tempElement = 0;
+  int leftElement = start;
+  int rightElement = middle;
+
+  while (leftElement < middle && rightElement < end) {
+    if (array[leftElement] < array[rightElement]) {
+      tempArray[tempElement] = array[leftElement];
+      leftElement++;
+      tempElement++;
+    } else {
+      tempArray[tempElement] = array[rightElement];
+      rightElement++;
+      tempElement++;
+    }
+  }
+
+  while (leftElement < middle) {
+    tempArray[tempElement] = array[leftElement];
+    leftElement++;
+    tempElement++;
+  }
+  while (rightElement < end) {
+    tempArray[tempElement] = array[rightElement];
+    rightElement++;
+    tempElement++;
+  }
+
+  for (int i = 0; i < end - start; i++) {
+    array[start + i] = tempArray[i];
+  }
+}
+
 int main(int argc, char const *argv[]) {
-  int array[] = {23, 432423, 123, 231545, 21335, 534534, 36, 542435, 523452345, 643, 132, 432, 123, 543, 9876, 345, 324, 456, 3456, 523534, 34, 23, 34, 53, 4534, 5, 345, 435, 2, 5, 7, 5, 4546, 3, 5, 6534, 54};
+  // int array[] = {23, 432423, 123, 231545, 21335, 534534, 36, 542435, 523452345, 643, 132, 432, 123, 543, 9876, 345, 324, 456, 3456, 523534, 34, 23, 34, 53, 4534, 5, 345, 435, 2, 5, 7, 5, 4546, 3, 5, 6534, 54};
+  // int array[] = {2, 3, 4, 1, 2, 5, 11};
+  int array[] = {10, 12, 13, 4, 5, 7};
   int arraySize = sizeof(array) / sizeof(int);
 
-  printf("%d", arraySize);
+  merge(array, 0, arraySize);
   printf("Elementos em Ordem:\n");
   for (int i = 0; i < arraySize; i++) {
     printf("e[%i]: %d\n", i, array[i]);
