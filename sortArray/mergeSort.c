cf8cc0383e1dc5d8cfd6f571388f2a6dcac88a85
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2021-01-31 21:10:06 -0300

    merge sort finalizado

--- sortArray/mergeSort.c
+++ sortArray/mergeSort.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 
-void swap(int *elementA, int *elementB) {
-  int aux = *elementA;
-  *elementA = *elementB;
-  *elementB = aux;
-}
-
-void merge(int *array, int start, int end) {
-  int middle = (start + end) / 2;
+void merge(int *array, int start, int middle, int end) {
   int tempArray[end - start];
-  
   int tempElement = 0;
   int leftElement = start;
   int rightElement = middle;
@@ -42,13 +34,20 @@ void merge(int *array, int start, int end) {
   }
 }
 
+void mergeSort(int *array, int start, int end) {
+  if (end - start > 1) {
+    int middle = (start + end) / 2;
+    mergeSort(array, start, middle);
+    mergeSort(array, middle, end);
+    merge(array, start, middle, end);
+  }
+}
+
 int main(int argc, char const *argv[]) {
-  // int array[] = {23, 432423, 123, 231545, 21335, 534534, 36, 542435, 523452345, 643, 132, 432, 123, 543, 9876, 345, 324, 456, 3456, 523534, 34, 23, 34, 53, 4534, 5, 345, 435, 2, 5, 7, 5, 4546, 3, 5, 6534, 54};
-  // int array[] = {2, 3, 4, 1, 2, 5, 11};
-  int array[] = {10, 12, 13, 4, 5, 7};
+  int array[] = {23, 432423, 123, 231545, 21335, 534534, 36, 542435, 523452345, 643, 132, 432, 123, 543, 9876, 345, 324, 456, 3456, 523534, 34, 23, 34, 53, 4534, 5, 345, 435, 2, 5, 7, 5, 4546, 3, 5, 6534, 54};
   int arraySize = sizeof(array) / sizeof(int);
 
-  merge(array, 0, arraySize);
+  mergeSort(array, 0, arraySize);
   printf("Elementos em Ordem:\n");
   for (int i = 0; i < arraySize; i++) {
     printf("e[%i]: %d\n", i, array[i]);
